509b439dd0f5f19f1224f9d71485bb671a72b5e6
Kishor Kumar Kar <kishor.kumar@encora.com>
2025-07-05 01:48:18 +0530

    use of proxy for identity-service

---
+++ microservices/api-gateway/src/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const env = require("dotenv");
+const proxy = require("express-http-proxy");
+env.config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(express.json());
+//middleware
+
+const proxyOptions = {
+  proxyReqPathResolver: (req) => {
+    return req.originalUrl.replace(/^\/v1/, "/api");
+  },
+  proxyErrorHandler: function (err, res, next) {
+    console.log("------", err.message);
+    res
+      .status(400)
+      .json({ message: `Internal server error`, error: err.message });
+  },
+};
+
+app.use(
+  "/v1/users/",
+  proxy(process.env.IDENTITY_SERVICE_URL, {
+    ...proxyOptions,
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers["Content-Type"] = "application/json";
+      return proxyReqOpts;
+    },
+    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+      console.log(
+        `Response received from Identity service: ${proxyRes.statusCode}`,
+      );
+      return proxyResData;
+    },
+  }),
+);
+
+app.get("/", (req, res) => res.send("Hello API-Gateway!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
